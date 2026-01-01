0b776fee1ef9fb245b853b6ec8026a1ebc31bb54
Dan Dees <dand@appsmiths.com>
2026-01-01 14:51:18 +0700

    rm-pkg.sh

--- rm-pkg.sh
+++ rm-pkg.sh
@@ -1,2 +1,2 @@
 declare -r pkg="$(basename "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")"
-rm -rf ${ASV_AF_PORTS}/build/${pkg}-build-${aplatform} ${ASV_PLAT_PORTS}/${pkg}
+rm -rvf ${ASV_AF_PORTS}/build/${pkg}-build-${aplatform} ${ASV_PLAT_PORTS}/${pkg}
